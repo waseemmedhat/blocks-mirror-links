5a66bb37f373020a9700858e19910939062f8ffc
waseemmedhat <waseem.medhat@clin-pro.com>
2022-12-17 19:56:09 +0200

    content and style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container">
+	<h1><code>bl.ocks.org</code><br />Link Mirrors</h1>
+
+	<input type="text" placeholder="Enter bl.ocks.org URL" />
+
+	<h2>Mirrors</h2>
+	<ul>
+		<li />
+		<li />
+	</ul>
+
+	<p>
+		The official bl.ocks.org is currently down (more precisely, it redirects to GitHub Gists). This
+		tool simply generates link for bl.ocks.org mirrors
+	</p>
+</div>
+
+<style>
+	.container {
+		padding: 45px;
+		max-width: 1200px;
+		margin-inline: auto;
+	}
+
+	h1,
+	h2 {
+		font-weight: 400;
+	}
+
+	input {
+		height: 2rem;
+		width: min(35rem, 100%);
+		margin: 1rem 0;
+		padding: 0.2rem 0.5rem;
+		border-radius: 0.2rem;
+	}
+
+	p {
+		margin-top: 1.5rem;
+	}
+
+	li {
+		list-style-type: circle;
+	}
+</style>
